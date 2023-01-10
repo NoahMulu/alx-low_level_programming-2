9bdf5fa4d126e7e0e58447396f9c020fb5db3222
NoahMulu <noahmulu1993@gmail.com>
2023-01-10 06:44:21 -0800

    bravo0000

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -2,6 +2,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * str_concat - concatenates two strings
+ * @s1: string1
+ * @s2: string2
+ *
+ * Return: Pointer
+ */
 char *str_concat(char *s1, char *s2)
 {
 	int l1, l2, i, j;
--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -2,6 +2,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * free_grid - frees a 2 dimensional grid previously created by
+ * alloc_grid function.
+ * @grid: matrix double pointer
+ * @height: rows (pointer)
+ *
+ * Return: Nothing
+ */
 void free_grid(int **grid, int height)
 {
 	int i;
