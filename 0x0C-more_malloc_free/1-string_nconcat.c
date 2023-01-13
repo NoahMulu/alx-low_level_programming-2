c6750540029b3f3bc77d14655bab9e35ccbfe7e7
NoahMulu <noahmulu1993@gmail.com>
2023-01-13 00:38:20 -0800

    _calloc

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,33 +1,38 @@
 #include "main.h"
 #include <stdlib.h>
-
+#include <stdio.h>
 /**
- * _strdup - Returns a pointer to a newly-allocated space in memory
- *           containing a copy of the string given as parameter.
- * @str:string
- *
- * Return:char
+ * string_nconcat - a function that concatenates two strings.
+ * @s1 : pointer to string
+ * @s2 : pointer to string
+ * @n : integer
+ * Return: pointer to string
  */
-char *_strdup(char *str)
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
+
+
+
+	unsigned int x1, x2, i, j;
 	char *s;
-	int i, l = 0;
 
-	if (str == NULL)
-		return (NULL);
-
-	for (i = 0; str[i]; i++)
-		l++;
-
-	s = malloc(sizeof(char) * (l + 1));
-
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	for (x1 = 0; s1[x1] != '\0'; x1++)
+		;
+	for (x2 = 0; s2[x2] != '\0'; x2++)
+		;
+	if (n >= x2)
+		n = x2;
+	s = malloc(sizeof(char) * (x1 + n + 1));
 	if (s == NULL)
 		return (NULL);
-
-	for (i = 0; str[i]; i++)
-		s[i] = str[i];
-
-	s[l] = '\0';
-
+	for (i = 0; s1[i] != '\0'; i++)
+		s[i] = s1[i];
+	for (j = x1; j < x1 + n; j++)
+		s[j] = s2[j - x1];
+	s[j] = '\0';
 	return (s);
 }
